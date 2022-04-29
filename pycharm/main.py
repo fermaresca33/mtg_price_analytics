2dad22b663254133099b687a0b92b1d9b7138922
unknown <fermaresca@gmail.com>
2022-04-29 19:39:24 -0300

    Project change to suport pycharm and jupyter examples:
    	-Added a pycharm sub-project to be able to run the mtg_cards_fetch as a script
    		°Has it's own virtual env with pandas and dash
    	-Moved jupyterlab project to sub-folder of project
    		°Has it's own virtual env with: pandas, dash, jupyter-dash and ipywidgets.

---
+++ pycharm/main.py
@@ -0,0 +1,112 @@
+#Import libreries
+import requests
+import pandas as pd
+from datetime import datetime
+
+import sqlite3
+
+#Setup
+#---------------------------------------------
+#Current date
+now = datetime.now()
+date_time = str(now.strftime("%Y%m%d"))
+
+#Getting Bulk Data
+#---------------------------------------------
+#Downloading the full card json that is generated every day.
+#More info about it in: https://scryfall.com/docs/api/bulk-data
+def get_bulk_data(api_url):
+    return requests.get(api_url)
+
+def get_cards_dataframe(api_response):
+    #Translate the response object's content from bytes to dictionary object so
+    #it can be easily manipulate it
+    api_json = api_response.json() #converts from bytes to dictionary
+
+    #Get cards information from bulk data in the responsed url.
+    bulk_url_json = requests.get(api_json['download_uri'], allow_redirects=True)
+
+    #Save data to pandas
+    # Load json data to a pandas Data frame.
+    url_json = bulk_url_json.json()  # converts from bytes to dictionary
+    pandas_df = pd.DataFrame(url_json)
+
+    return pandas_df
+
+def df_general_clean(cards_df):
+    columns_list = ['id', 'name', 'image_uris', 'mana_cost', 'cmc', 'power',
+                    'toughness', 'reserved', 'foil', 'nonfoil', 'set_id',
+                    'set_name', 'rarity', 'frame', 'prices', 'date_time']
+
+    #General Cleaning
+    #---------------------------------------------------
+    #Discard any card that's not in english
+    cards_df_flt = cards_df.loc[cards_df["lang"] == "en"]
+    #Discard the cards without image
+    cards_df_flt = cards_df_flt.dropna(subset=['image_uris'])
+    #Adds date_time to the Data frame
+    cards_df_flt['date_time'] = date_time
+    #Keep only necessary columns
+    cards_df_flt = cards_df_flt[columns_list]
+
+    #Split dictionary-like columns
+    #---------------------------------------------------
+    #Split dictonary column for cards images
+    cards_df_flt = cards_df_flt.join(cards_df_flt.image_uris.apply(pd.Series), how='left')
+    #Split dictonary column for cards prices
+    cards_df_flt = cards_df_flt.join(cards_df_flt.prices.apply(pd.Series), how='left')
+    #Drop innecesary columns after de split
+    cards_df_flt.drop(["image_uris", "prices", "large", "png", "art_crop", "border_crop", "tix"],
+                      axis=1, inplace=True)
+    return cards_df_flt
+
+def connect_to_sqlite_db(db_path):
+    #Generates (or creates if it doesn't exist) the db connection.
+    conn = sqlite3.connect(db_path)
+    return conn
+
+def disconnect_from_sqlite_db(conn):
+    #Close connection
+    conn.close()
+    return
+
+def save_cards_to_local_db(cards_df):
+    #Perform a general clean before storage
+    clean_cards_df = df_general_clean(cards_df)
+
+    #Divide dataframe into two different tables
+    #   - scryfall_cards: Holds the cards static info(name, set, images, rarity, etc)
+    #   - scryfall_daily_prices : Holds the cards columns variable in a daily basis
+    #       (regular card price, foil price, etc)
+    prices_df = clean_cards_df.copy(deep=True)
+    prices_df = prices_df[['id', 'usd', 'usd_foil', 'usd_etched', 'eur', 'eur_foil', 'date_time']]
+    prices_df.rename(columns={'id': 'card_id'}, inplace=True)
+
+    cards_static_info_df = clean_cards_df.copy(deep=True)
+    cards_static_info_df = cards_static_info_df[['id', 'name', 'mana_cost', 'cmc', 'power', 'toughness',
+                                                 'reserved', 'foil', 'nonfoil', 'set_id', 'set_name',
+                                                 'rarity', 'frame', 'small', 'normal', 'date_time']]
+    cards_static_info_df.rename(columns={'id': 'card_id', 'date_time': 'last_update'}, inplace=True)
+
+    #Connect to local DB
+    # the .. indicates to go back a directory.
+    # if it's at the same level then use "sqlite_db/mtg_cards.db" insted.
+    conn = connect_to_sqlite_db("../sqlite_db/mtg_cards.db")
+
+    #Load pandas dataframe into scryfall_cards table. Append Because it will add new prices each day.
+    prices_df.to_sql(name="scryfall_daily_prices", con=conn, if_exists="append", index=False)
+    #Load pandas dataframe into scryfall_cards table. Replace existing because it's static info.
+    cards_static_info_df.to_sql(name="scryfall_cards", con=conn, if_exists="replace", index=False)
+
+    #Close connection
+    disconnect_from_sqlite_db(conn)
+
+if __name__ == '__main__':
+    #Get the api response for accessing the bulk data.
+    api_response = get_bulk_data('https://api.scryfall.com/bulk-data/default-cards')
+    print(api_response.status_code)  #should be 200=ok
+
+    #Obtain cards dataframe from the response
+    cards_bulk_df = get_cards_dataframe(api_response)
+
+    save_cards_to_local_db(cards_bulk_df)
